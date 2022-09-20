da1529e3682a76b2d690e1ed6e3d6319f06bef6e
tekin <gundogdutekin@gmail.com>
2022-09-20 15:35:06 +0300

    Created to Envoriment Files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
+      <h1>{process.env.REACT_APP_API_URL}</h1>
      <img src="./logo192.png" alt="" />
      <img src={logo} alt="" />
     </div>
