7ce689637d0d7ca3d7b556d4bda0fb646c395440
tekin <gundogdutekin@gmail.com>
2022-09-20 17:24:21 +0300

    Bootstrap components installed and used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Test from './Test';
 import styles from "./App.module.css";
+import Bootstrap from './Bootstrap';
 function App() {
   return (
     <div className={styles.App}>
       <h1>{process.env.REACT_APP_API_URL}</h1>
     <Test/>
+    <Bootstrap/>
     </div>
     
   );
---
+++ src/Bootstrap.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+const Bootstrap = () => {
+    return (
+        <div>
+            <h3>Botstarp Componentleri</h3>
+            <Button>Bootstrap</Button>
+        </div>
+    )
+}
+
+export default Bootstrap
