b8a412a6ebd50b2a3b382b4fa2e5b9246b406e63
tekin <gundogdutekin@gmail.com>
2022-09-20 17:57:18 +0300

    Tailwindcss  components installed and used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Test from './Test';
 import styles from "./App.module.css";
 import Bootstrap from './Bootstrap';
+import './index.css';
+import Tailwind from './Tailwind';
+
 function App() {
   return (
     <div className={styles.App}>
       <h1>{process.env.REACT_APP_API_URL}</h1>
     <Test/>
     <Bootstrap/>
+    <Tailwind/>
     </div>
     
   );
---
+++ src/Tailwind.js
@@ -0,0 +1,16 @@
+const Tailwind=()=>{
+    return(
+        <>
+        <div className="bg-gray-200">
+            <h1 className="text-4xl text-center">Tailwind CSS</h1>
+           
+        </div>
+        <div className="flex justify-center">
+         <button className="text-center bg-red-400 px-10 rounded-full bg-red rounded-sm pointer-events-auto ">Text</button>
+        </div>
+        </>
+    )
+
+}
+
+export default Tailwind
